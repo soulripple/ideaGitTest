4c374ba5d65daa04d0d06c31ff5014e1c4aa1e6b
soulripple <997336675@qq.com>
2019-08-02 17:08:38 +0800

    添加了一个方法

--- maven_javaweb/src/main/java/com/itheima/GitTest.java
+++ maven_javaweb/src/main/java/com/itheima/GitTest.java
@@ -7,4 +7,9 @@ public class GitTest {
         System.out.println("hello git test!");
         System.out.println("hello git test!");
     }
+    public void test(){
+        int a=10;
+        int b=2;
+        System.out.println(a/b);
+    }
 }
