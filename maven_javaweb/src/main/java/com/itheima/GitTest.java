b93e07db5e5b24f2a22809d656c046cf574a1047
soulripple <997336675@qq.com>
2019-08-02 17:26:55 +0800

    添加注释

--- maven_javaweb/src/main/java/com/itheima/GitTest.java
+++ maven_javaweb/src/main/java/com/itheima/GitTest.java
@@ -6,8 +6,9 @@ public class GitTest {
         System.out.println("hello git test!");
         System.out.println("hello git test!");
         System.out.println("hello git test!");
+        //hello test
     }
-    public void test(){
+    public static  void  test(){
         int a=10;
         int b=2;
         System.out.println(a/b);
