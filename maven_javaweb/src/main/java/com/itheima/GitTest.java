ca2ed1671f209bcba1d3ad9c563f7a951ad74747
soulripple <997336675@qq.com>
2019-08-02 17:01:43 +0800

    把本地工程添加到git

---
+++ maven_javaweb/src/main/java/com/itheima/GitTest.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git test!");
+        System.out.println("hello git test!");
+        System.out.println("hello git test!");
+        System.out.println("hello git test!");
+    }
+}
